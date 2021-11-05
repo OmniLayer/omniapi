949c1c17d41e56f19fb5469b514fe74b1055beb0
Adam <adam@mastercoin.org>
2021-11-05 13:28:42 +0000

    add blockhash to revision output

--- api/common.py
+++ api/common.py
@@ -47,9 +47,9 @@ def raw_revision():
     print_debug(("cache looked success",ckey),7)
   except:
     print_debug(("cache looked failed",ckey),7)
-    ROWS=dbSelect("select blocknumber, blocktime from blocks order by blocknumber desc limit 1")
+    ROWS=dbSelect("select blocknumber, blocktime,blockhash from blocks order by blocknumber desc limit 1")
     ROWT=dbSelect("select updated_at from settings where key='parserLastRun'")
-    response = {'last_block': ROWS[0][0], 'block_time': str(ROWS[0][1]), 'last_parsed':str(ROWT[0][0])}
+    response = {'last_block': ROWS[0][0], 'block_time': str(ROWS[0][1]), 'block_hash': str(ROWS[0][2]), 'last_parsed':str(ROWT[0][0])}
     #cache 1 min
     lSet(ckey,json.dumps(response))
     lExpire(ckey,60)
